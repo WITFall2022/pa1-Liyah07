b77216fc4b569515efaa9b5500efa4d71a99f491
337886 <337886@664-161465-LAB.schools.mybps.org>
2022-10-31 10:25:10 -0400

    I completed my PA1

--- src/edu/wit/cs/comp1000/PA1b.java
+++ src/edu/wit/cs/comp1000/PA1b.java
@@ -1,11 +1,35 @@
 package edu.wit.cs.comp1000;
 
+import java.util.Scanner;
+
 // TODO: document this class
 public class PA1b {
 
 	// TODO: document this function
 	public static void main(String[] args) {
 		// TODO: write your code here
+Scanner input = new Scanner(System.in);
+		
+		
+		int totalinches;
+		System.out.printf("Enter number of inches: ");
+		
+		totalinches = input.nextInt();
+		
+		int yards;
+		
+		yards = totalinches/36;
+		
+		System.out.printf("yards:%d%n", yards);
+		int feet;
+		feet = totalinches%36/12;
+		
+		 System.out.printf("feet:%d%n", feet);
+		 
+		 int inches;
+			inches = totalinches%36%12/1;
+			
+			 System.out.printf("inches:%d%n", inches);
 	}
 
 }
